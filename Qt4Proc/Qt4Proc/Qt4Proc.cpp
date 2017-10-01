0a491b64d7e37a83daec8b1350e6f77d66e69028
attilakrupl <krupl.attila@gmail.com>
2017-10-01 14:52:28 +0200

    Both Projects succesfully built

--- Qt4Proc/Qt4Proc/Qt4Proc.cpp
+++ Qt4Proc/Qt4Proc/Qt4Proc.cpp
@@ -1,12 +1,12 @@
-#include "Qt4Proc.h"
+#include "qt4proc.h"
 
-
-
-Qt4Proc::Qt4Proc()
+Qt4Proc::Qt4Proc(QWidget *parent, Qt::WFlags flags)
+	: QMainWindow(parent, flags)
 {
+	ui.setupUi(this);
 }
-
 
 Qt4Proc::~Qt4Proc()
 {
+
 }
--- Qt4Proc/Qt4Proc/Qt4Proc.h
+++ Qt4Proc/Qt4Proc/Qt4Proc.h
@@ -1,8 +1,19 @@
-#pragma once
-class Qt4Proc
+#ifndef QT4PROC_H
+#define QT4PROC_H
+
+#include <QtGui/QMainWindow>
+#include "ui_qt4proc.h"
+
+class Qt4Proc : public QMainWindow
 {
+	Q_OBJECT
+
 public:
-	Qt4Proc();
+	Qt4Proc(QWidget *parent = 0, Qt::WFlags flags = 0);
 	~Qt4Proc();
+
+private:
+	Ui::Qt4ProcClass ui;
 };
 
+#endif // QT4PROC_H
